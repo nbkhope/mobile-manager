6a156b6092ec323279c9cb453ffda9a4a426f42d
nbkhope <ryumemaru@yahoo.com>
2016-10-29 15:55:18 -0700

    Add EmployeeList and add new Scene to router to point to it

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Somebody</Text>
+        <Text>Somebody</Text>
+        <Text>Somebody</Text>
+        <Text>Somebody</Text>
+        <Text>Somebody</Text>
+        <Text>Somebody</Text>
+        <Text>Somebody</Text>
+        <Text>Somebody</Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeList;
