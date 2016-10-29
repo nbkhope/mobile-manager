b59ce73dffdb987f1e9e8d4e469aa32c9552a72c
nbkhope <ryumemaru@yahoo.com>
2016-10-28 20:38:48 -0700

    Add emailChanged and passwordChanged action creators; use emailChanged in LoginForm

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+// Import action types
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED
+} from './types';
+
+export const emailChanged = (email) => {
+  return {
+    type: EMAIL_CHANGED,
+    payload: email
+  };
+};
+
+export const passwordChanged = (password) => {
+  return {
+    type: PASSWORD_CHANGED,
+    payload: password
+  };
+};
