f2ce709cd3bc25f9eaa0666d1d12a3a0a99ce7a5
nbkhope <ryumemaru@yahoo.com>
2016-10-29 19:58:52 -0700

    Add EmployeeCreate and route to it from EmployeeList

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
@@ -15,7 +16,12 @@ const RouterComponent = () => {
           component={EmployeeList}
           title="Employees"
           rightTitle="Add"
-          onRight={() => null}
+          onRight={() => Actions.employeeCreate()}
+        />
+        <Scene
+          key="employeeCreate"
+          component={EmployeeCreate}
+          title="Create Employee"
         />
       </Scene>
     </Router>
