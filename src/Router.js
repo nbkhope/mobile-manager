68041cd0e2813206c5f33e71f3bb957b9200426c
nbkhope <ryumemaru@yahoo.com>
2016-10-29 19:59:23 -0700

    Make employeeList initial scene

--- src/Router.js
+++ src/Router.js
@@ -17,6 +17,7 @@ const RouterComponent = () => {
           title="Employees"
           rightTitle="Add"
           onRight={() => Actions.employeeCreate()}
+          initial
         />
         <Scene
           key="employeeCreate"
