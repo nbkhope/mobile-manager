5c56bef0bb60e0dc53c2868c90009ad39bd5b852
nbkhope <ryumemaru@yahoo.com>
2016-10-30 20:38:56 -0700

    Update employeeCreate to write to the Firebase database

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,3 +1,6 @@
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+
 import {
   EMPLOYEE_UPDATE
 } from './types';
@@ -10,5 +13,15 @@ export const employeeUpdate = ({ prop, value }) => {
 };
 
 export const employeeCreate = ({ name, phone, shift }) => {
-  
+  // Get access to the currently authenticated user
+  const { currentUser } = firebase.auth();
+  console.log('The current user is', currentUser);
+
+  return () => {
+    // Add new employee data to the database
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift })
+      // Go back to list; use type: 'reset' so that there's no back button
+      .then(() => Actions.employeeList({ type: 'reset' }));
+  };
 };
