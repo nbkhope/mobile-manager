7a52f28125d3ab125f08cf8f781c8de78846bc76
nbkhope <ryumemaru@yahoo.com>
2016-10-31 11:28:24 -0700

    Add missing import for EmployeeEdit

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
+import EmployeeEdit from './components/EmployeeEdit';
 
 const RouterComponent = () => {
   return (
