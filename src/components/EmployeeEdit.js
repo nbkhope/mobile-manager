698136975196e3d53dba7fd210e448ac02bd2d5b
nbkhope <ryumemaru@yahoo.com>
2016-10-31 14:01:56 -0700

    Add Yes/No modal to confirm employee removal

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -2,11 +2,16 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import Communications from 'react-native-communications';
 import { connect } from 'react-redux';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 import { employeeEdit, employeeUpdate } from '../actions';
 import EmployeeForm from './EmployeeForm';
 
 class EmployeeEdit extends Component {
+  // Initial component state
+  state = {
+    showModal: false
+  };
+
   componentWillMount() {
     // the employee is injected when an employee list item is clicked
     _.each(this.props.employee, (value, prop) => {
@@ -28,6 +33,11 @@ class EmployeeEdit extends Component {
     Communications.text(phone, `Your upcoming shift is on ${shift}.`);
   }
 
+  toggleModal() {
+    const showModal = !this.state.showModal;
+    this.setState({ showModal });
+  }
+
   render() {
     return (
       <Card>
@@ -42,6 +52,20 @@ class EmployeeEdit extends Component {
             Text Schedule
           </Button>
         </CardSection>
+
+        <CardSection>
+          <Button onPress={() => this.toggleModal()}>
+            Remove
+          </Button>
+        </CardSection>
+
+        <Confirm
+          visible={this.state.showModal}
+          onAccept={() => null}
+          onDecline={() => this.toggleModal()}
+        >
+          Are you sure you want to perform this action?
+        </Confirm>
       </Card>
     );
   }
