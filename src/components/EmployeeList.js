74d21c023e717a0d66dec4874163fc1c094c40e0
nbkhope <ryumemaru@yahoo.com>
2016-10-30 21:30:23 -0700

    Add mapStateToProps and mapDispatchToProps to EmlpoyeeList and use action creator employeesFetch

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
+  componentWillMount() {
+    this.props.employeesFetch();
+  }
+
   render() {
     return (
       <View>
-        <Text>Somebody</Text>
-        <Text>Somebody</Text>
-        <Text>Somebody</Text>
-        <Text>Somebody</Text>
-        <Text>Somebody</Text>
-        <Text>Somebody</Text>
-        <Text>Somebody</Text>
         <Text>Somebody</Text>
       </View>
     );
   }
 }
 
-export default EmployeeList;
+const mapStateToProps = (state) => {
+  // the state itself is an object with all employees
+  return state;
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
